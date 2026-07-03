09641db010286e97435161b9ab9fabcc4e3231e2
Nik-i-Net <netesnik02@gmail.com>
2026-07-03 14:44:56 +0300

    feat: add cart badge and close cart on clicking outside or `<esc>`

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const cartPopup = document.querySelector(".cart-popup");
 const cartBtn = document.querySelector(".header-cart-btn");
-cartBtn.addEventListener("click", function () {
-  cartPopup.classList.toggle("hidden");
-});
-
 const lightboxOverlay = document.querySelector(".lightbox-overlay");
 const lightboxContainer = document.querySelector(".lightbox-container");
 const lightboxCloseBtn = document.querySelector(".lightbox-close");
 const productGalleryBtn = document.querySelector(".product-gallery-btn");
+
+cartBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  cartPopup.classList.toggle("hidden");
+});
+
+document.addEventListener("click", function () {
+  cartPopup.classList.add("hidden");
+});
+
+cartPopup.addEventListener("click", (e) => e.stopPropagation());
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    cartPopup.classList.add("hidden");
+    lightboxOverlay.classList.add("hidden");
+  }
+});
 
 productGalleryBtn.addEventListener("click", function () {
   lightboxOverlay.classList.remove("hidden");
@@ -15,12 +29,6 @@ productGalleryBtn.addEventListener("click", function () {
 
 lightboxCloseBtn.addEventListener("click", function () {
   lightboxOverlay.classList.add("hidden");
-});
-
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") {
-    lightboxOverlay.classList.add("hidden");
-  }
 });
 
 lightboxOverlay.addEventListener("click", function () {
